9adc1511dc575c4dcfd67271d2104ad9c954e728
Manuel Gass <mnl.gass@gmail.com>
2026-03-06 16:28:57 +0100

    functionality complete, front-end like MS-DOS

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get "lists/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: "lists#index"
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
@@ -8,6 +10,15 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+
+  # 1. A user can see all the lists
+  # 2. A user can see the details of a given list and its name
+  resources :lists, only: [ :show, :new, :create ] do
+    resources :bookmarks, only: [:new, :create]
+  end
+
+  resources :bookmarks, only: [:destroy]
+
 
   # Defines the root path route ("/")
   # root "posts#index"
